402db64e6e4b3ef9f99d3a233f272ca1592e9379
encho253 <encho.enevski@gmail.com>
2019-01-15 01:02:00 +0200

    Added new class libraries(Data and Models)

--- OnLineVideotech/src/Data/OnLineVideotech.Data/OnLineVideotechContext.cs
+++ OnLineVideotech/src/Data/OnLineVideotech.Data/OnLineVideotechContext.cs
@@ -1,11 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using OnLineVideotech.Web.Areas.Identity.Data;
 
 namespace OnLineVideotech.Web.Models
 {
---
+++ OnLineVideotech/src/Data/OnLineVideotech.Models/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OnLineVideotech.Models
+{
+    public class Class1
+    {
+    }
+}
