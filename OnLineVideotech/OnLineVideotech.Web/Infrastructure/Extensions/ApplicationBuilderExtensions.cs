237dc7435fa887b47d24212e01520ab9c3210858
encho253 <encho.enevski@gmail.com>
2019-01-24 01:09:31 +0200

    added new project structure

---
+++ OnLineVideotech/OnLineVideotech.Web/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace OnLineVideotech.Web.Infrastructure.Extensions
+{
+    public static class ApplicationBuilderExtensions
+    {
+        //public static IApplicationBuilder UseDatabaseMigration(this IApplicationBuilder app)
+        //{
+        //    using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScope>)
+        //    {
+
+        //    }
+        //}
+    }
+}
